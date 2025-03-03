6893ea1fe1babacd81016abf5f83bc3ba8bc7c2b
anderortiz2019 <149614829+anderortiz2019@users.noreply.github.com>
2025-03-03 07:48:21 +0000

    Deploying to gh-pages from @ anderortiz2019/anderortiz2019.github.io@eb0b8188203598b1d05b0c6c1b874507a63cc66e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -429,15 +429,15 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-coming-soon",
           title: 'Coming Soon!',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "news-coming-soon-long-form-template",
+          title: 'COMING SOON (Long-form template)',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
